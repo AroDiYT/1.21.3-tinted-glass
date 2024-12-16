b36fb93ed7984e4aa14c533ba93a5babfde74edf
AroDiYT <dariovanharneveldt@gmail.com>
2024-12-16 12:56:10 +0100

    fix: Icon swapped to Pickaxe in tools tab

--- src/main/java/org/vanharneveldt/tinted_glass/item/ModCreativeModeTabs.java
+++ src/main/java/org/vanharneveldt/tinted_glass/item/ModCreativeModeTabs.java
@@ -31,7 +31,7 @@ public class ModCreativeModeTabs {
 
     public static final Supplier<CreativeModeTab> TINTED_TOOLS = CREATIVE_MODE_TAB.register("tinted_glass_tools_tab",
             () -> CreativeModeTab.builder()
-                    .icon(() -> new ItemStack(ModItems.RAWALUMINIUM.get()))
+                    .icon(() -> new ItemStack(ModItems.ALUMINIUMPICKAXE.get()))
                     .title(Component.translatable("creativetab.revampedglass.tinted_tools"))
                     .displayItems(
                             (itemDisplayParameters, output) -> {
